a849fb53fbb65da9329aff238722513951d0cbbd
Feotov Daniil <hairyhum@gmail.com>
2015-06-03 17:36:26 +0300

    Multicast clustering

--- src/ejabberd_router_multicast.erl
+++ src/ejabberd_router_multicast.erl
@@ -203,7 +203,7 @@ do_route(From, Domain, Destinations, Packet) ->
 	[] -> do_route_normal(From, Destinations, Packet);
 
 	%% If available, send the packet using multicast service
-	[R] ->
+	[R|_] ->
 	    case R#route_multicast.pid of
 		Pid when is_pid(Pid) ->
 		    Pid ! {route_trusted, From, Destinations, Packet};
